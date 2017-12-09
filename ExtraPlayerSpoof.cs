6a82faf8ad0d9bf0ba39900996bfcb4918715cdc
ExtraConcentratedJuice <onomatopoeicboom@gmail.com>
2017-12-09 13:29:06 -0800

    why am i putting a one line plugin on github

---
+++ ExtraPlayerSpoof.cs
@@ -0,0 +1,30 @@
+﻿using Steamworks;
+using Rocket.Core.Plugins;
+using Rocket.Core.Logging;
+
+namespace ExtraConcentratedJuice.ExtraPlayerSpoof
+{
+    public class ExtraPlayerSpoof : RocketPlugin<ExtraPlayerSpoofConfig>
+    {
+        protected override void Load()
+        {
+            Logger.Log("-----------");
+            Logger.Log("Extra's Player Spoofer Loaded!");
+            Logger.Log("Can you believe it? People are selling this one line plugin for $6!");
+            Logger.Log("> Setting player count to: " + Configuration.Instance.player_count);
+            Logger.Log("-----------");
+
+            SteamGameServer.SetBotPlayerCount(Configuration.Instance.player_count);
+        }
+
+        protected override void Unload()
+        {
+        }
+
+        private void OnLevelLoaded(int l)
+        {
+            SteamGameServer.SetBotPlayerCount(Configuration.Instance.player_count);
+        }
+
+    }
+}
---
+++ ExtraPlayerSpoofConfig.cs
@@ -0,0 +1,14 @@
+﻿using Rocket.API;
+
+
+namespace ExtraConcentratedJuice.ExtraPlayerSpoof
+{
+    public class ExtraPlayerSpoofConfig : IRocketPluginConfiguration
+    {
+        public int player_count;
+        public void LoadDefaults()
+        {
+            player_count = 69;
+        }
+    }
+}
